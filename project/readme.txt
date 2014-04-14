7f554c9637fb1f76f8d225612ca5712055294aa2
Jan Hanák <Honzales@gmail.com>
2014-04-14 15:57:28 +0200

    bla

---
+++ project/readme.txt
@@ -0,0 +1,3 @@
+název projektu: Bludiště
+název týmu: grp062
+členové: Jan Hanák - vedoucí, Pavlína Bortlová - člen
---
+++ project/run.sh
@@ -0,0 +1,2 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+java -cp $DIR/dest-server -jar $DIR/dest-server/bludiste.jar
